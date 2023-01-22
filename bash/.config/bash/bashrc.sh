d76af4ffe35df715cb84bae82f0be4b7c10f95c0
t3hmun <git@t3hmun.com>
2023-01-22 00:43:08 +0000

    Annotate bash config.

--- bash/.config/bash/bashrc.sh
+++ bash/.config/bash/bashrc.sh
@@ -1,20 +1,25 @@
 # This is meant to be sourced at the bottom of the default .bashrc.
 # I'm not using stow to overwrite .bashrc because the default stuff the os generates is fine, maybe.
-#
 
 
+# This detects WSL. w is my work folder in windows.
 if [[ "$(uname -r)" == *"microsoft"* ]]; then
   alias w='cd /mnt/w/'
-else
-  alias w='cd ~/w/'
 fi
 
+# Vertical list, all files, human file sizes.
 alias ll='ls -lah'
+# I use this to list all the interesting files in a project. TODO write a program that limits depth / size.
 alias f='fd -H -E node_modules -E .git'
+# Lunar Vim is better than vscode.
 alias lv='lvim'
-alias p='cd ~/p'
+# My favorite folders, instantly.
 alias h='cd ~'
+alias p='cd ~/p'
+
+# This is the complete way to reload bash with all the env, without just running bash in bash - look at the pstree.
 alias reload='exec bash --login'
+
 
 # Activate Starship prompt.
 command -v starship >/dev/null 2>&1 && eval "$(starship init bash)"
